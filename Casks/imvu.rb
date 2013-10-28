fa708fb776ae9835df806a94cd8acfb622ca058b
Forrest Bowman <sexymacman@me.com>
2013-10-27 17:11:08 -0700

    IMVU Client 493.32 as a cask
    added IMVU 493.32 cask

---
+++ Casks/imvu.rb
@@ -0,0 +1,7 @@
+class Imvu < Cask
+  url 'http://static-akm.imvu.com/imvufiles/installers/IMVU-493.32.dmg'
+  homepage 'http://imvu.com'
+  version '493.32'
+  sha1 '92740f983544f30cab2a9b48821e85e15e22c9d2'
+  link 'IMVU.app'
+end
