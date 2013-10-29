129c5dc5e1e5582cceb933ac47c8313f2fc61d50
Forrest Bowman <sexymacman@me.com>
2013-10-28 22:15:57 -0700

    adding a cask for camtwist 2.4 and hexfiend

---
+++ Casks/camtwist.rb
@@ -0,0 +1,7 @@
+class Camtwist < Cask
+  url 'http://camtwiststudio.com/release/CamTwist_2.4.dmg'
+  homepage 'http://camtwiststudio.com'
+  version '2.4'
+  sha1 '99f8f3fc9d27bf4e532d5330c1d0637b743efea8'
+  install 'Camtwist.pkg'
+end
